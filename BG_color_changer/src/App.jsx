fb93d3c66b2b4931fb6502e3b81802d1e9de5fd6
Nandkishor Rathore <nrr.tvans@gmail.com>
2024-03-23 16:44:08 +0530

    color button div, justify and center at bottom

--- BG_color_changer/src/App.jsx
+++ BG_color_changer/src/App.jsx
@@ -1,37 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("pink");
 
   return (
     <div
-      className="w-full h-screen duration-200 "
-      style={{ backgroundColor: color }}>
-        <div className="m-3 gap-2 fixed flex flex-wrap bg-white rounded-3xl p-2 justify-center">
-          <button className="bg-red rounded-2xl p-2 justify-center"
+      className="w-full h-screen flex ... duration-200 flex flex-wrap justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <div className="m-3 gap-2 fixed flex flex-wrap justify-center bottom-12 px-2 bg-white rounded-3xl p-2 flex flex-wrap justify-center">
+        <button
+          className="flex-auto bg-red rounded-2xl p-2 justify-center"
           style={{ backgroundColor: "red" }}
-          onClick={()=>setColor("red")}
-          >Red</button>
-          <button className="bg-red rounded-2xl p-2 justify-center"
+          onClick={() => setColor("red")}
+        >
+          Red
+        </button>
+        <button
+          className="flex-auto bg-red rounded-2xl p-2 justify-center"
           style={{ backgroundColor: "green" }}
-          onClick={()=>setColor("green")}
-          >Green</button>
-          <button className="bg-red rounded-2xl p-2 justify-center"
+          onClick={() => setColor("green")}
+        >
+          Green
+        </button>
+        <button
+          className="flex-auto bg-red rounded-2xl p-2 justify-center"
           style={{ backgroundColor: "blue" }}
-          onClick={()=>setColor("blue")}
-          >Blue</button>
-          <button className="bg-red rounded-2xl p-2 justify-center"
+          onClick={() => setColor("blue")}
+        >
+          Blue
+        </button>
+        <button
+          className="flex-auto bg-red rounded-2xl p-2 justify-center"
           style={{ backgroundColor: "yellow" }}
-          onClick={()=>setColor("yellow")}
-          >Yellow</button>
-          <button className="bg-red rounded-2xl p-2 justify-center"
+          onClick={() => setColor("yellow")}
+        >
+          Yellow
+        </button>
+        <button
+          className="flex-auto bg-red rounded-2xl p-2 justify-center"
           style={{ backgroundColor: "grey" }}
-          onClick={()=>setColor("grey")}
-          >Grey</button>
-        </div>
+          onClick={() => setColor("grey")}
+        >
+          Grey
+        </button>
+      </div>
     </div>
   );
 }
